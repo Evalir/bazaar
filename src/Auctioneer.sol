816d657736cbd6682be3e4dd916539a1e95cb2a4
Enrique Ortiz <hi@enriqueortiz.dev>
2023-03-09 22:12:44 -0400

    feat: keep on with Auctioneer structure

--- src/Auctioneer.sol
+++ src/Auctioneer.sol
@@ -1,20 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import {IERC20} from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+address constant NATIVE_ASSET = address(0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
+
 contract Auctioneer {
     /*//////////////////////////////////////////////////////////////
                                STORAGE
     //////////////////////////////////////////////////////////////*/
+    /// @dev The highest bidder, so far.
     address public highestBidder;
+    /// @dev The auction start time.
+    /// The auction in total lasts `bidPeriodDuration + revealPeriodDuration`.
+    /// Therefore the auction end time is `auctionStartTime + bidPeriodDuration + revealPeriodDuration`.
+    uint256 public auctionStartTime;
+    /// @dev The bid period duration after deployment.
     uint256 public bidPeriodDuration;
+    /// @dev The reveal period duration after the bid period.
     uint256 public revealPeriodDuration;
+
+    /// @dev The token used for bidding.
+    IERC20 public bidToken;
 
     event Bid(address indexed bidder, uint256 amount);
 
     error BidPeriodNotOver();
     error RevealPeriodNotOver();
+    error ZeroBidTime();
+    error ZeroReavealTime();
 
     constructor(uint256 _bidPeriodDuration, uint256 _revealPeriodDuration) {
+        if (_bidPeriodDuration == 0) revert ZeroBidTime();
+        if (_revealPeriodDuration == 0) revert ZeroReavealTime();
         bidPeriodDuration = _bidPeriodDuration;
         revealPeriodDuration = _revealPeriodDuration;
     }
